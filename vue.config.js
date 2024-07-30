c66612a5e8d3c8eae4f4115977dc078b6bb201dd
stone meng <370025263@qq.com>
2024-07-30 18:36:21 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   lintOnSave: false,
-  transpileDependencies: true,
+  transpileDependencies: [],  // 现在设置为一个空数组
   publicPath: process.env.NODE_ENV === 'production'
     ? '/rl_ladder/'
     : '/'
