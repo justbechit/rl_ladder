d43cd061a3d5d0bf4bf98aec6976d86a4d3a1ff0
370025263 <370025263@qq.com>
2024-07-31 01:37:36 +0800

    fuck ai

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   lintOnSave: false,
   transpileDependencies: [],  // 现在设置为一个空数组
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/rl_ladder/'
-    : '/'
+  publicPath: '/'
 }
